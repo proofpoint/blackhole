86b658cf057c876b59cada5efcad7f913bdfab0c
David Hagar <dhagar@proofpoint.com>
2011-10-06 11:21:23 -0700

    Initial commit

---
+++ src/main/java/com/proofpoint/anomalytics/MainModule.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.anomalytics;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import org.weakref.jmx.guice.MBeanModule;
+
+import static com.proofpoint.configuration.ConfigurationModule.bindConfig;
+import static com.proofpoint.discovery.client.DiscoveryBinder.discoveryBinder;
+import static com.proofpoint.event.client.EventBinder.eventBinder;
+
+public class MainModule
+        implements Module
+{
+    public void configure(Binder binder)
+    {
+        binder.requireExplicitBindings();
+        binder.disableCircularProxies();
+
+        binder.bind(DummyTrafficStatsResource.class).in(Scopes.SINGLETON);
+
+        discoveryBinder(binder).bindHttpAnnouncement("trafficstats");
+    }
+}
